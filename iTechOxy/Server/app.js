2414abb7ac0d02e653425edfac003dd8d5b7babc
dzmitryu <dulezla@gmail.com>
2017-06-17 19:38:08 +0300

    Added separated routing

--- iTechOxy/Server/app.js
+++ iTechOxy/Server/app.js
@@ -13,23 +13,7 @@ dbManager.getConnection().then((db) => {
     models = db.models;
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
-
-app.post('/api/v1/token', function (req, res) {
-    console.log(req.body);
-    models.user.findAll().then((result) => {
-        console.log(result);
-    });
-
-    res.status(200).json({message: 'Saved', success: true});
-});
-
-app.put('/api/v1/oxygen', function (req, res) {
-    console.log(req.body);
-    res.status(200).json({message: 'Logged', success: true});
-});
+require('./routes')(app);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
---
+++ iTechOxy/Server/routes/index.js
@@ -0,0 +1,48 @@
+let UserController = require('../controllers/user');
+let OxygenController = require('../controllers/oxygen');
+
+let _ = require('underscore');
+
+let userController = new UserController();
+let oxygenController = new OxygenController();
+
+
+let routes = [
+    {
+        path: '/api/v1/login',
+        httpMethod: 'POST',
+        middleware: [userController.login]
+    },
+    {
+        path: '/api/v1/token',
+        httpMethod: 'POST',
+        middleware: [userController.updateToken]
+    },
+    {
+        path: '/api/v1/oxygen',
+        httpMethod: 'PUT',
+        middleware: [oxygenController.log]
+    }
+];
+
+module.exports = function (app) {
+    _.each(routes, function (route) {
+        let args = _.flatten([route.path, route.middleware]);
+        switch (route.httpMethod.toUpperCase()) {
+            case 'GET':
+                app.get.apply(app, args);
+                break;
+            case 'POST':
+                app.post.apply(app, args);
+                break;
+            case 'PUT':
+                app.put.apply(app, args);
+                break;
+            case 'DELETE':
+                app.delete.apply(app, args);
+                break;
+            default:
+                throw new Error('Invalid HTTP method specified for route ' + route.path);
+        }
+    });
+};
