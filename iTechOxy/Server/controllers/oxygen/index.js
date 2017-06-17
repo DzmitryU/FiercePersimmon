1a1cd3927ba090fea383e460343c72fa9ec9237f
dzmitryu <dulezla@gmail.com>
2017-06-17 22:45:03 +0300

    Oxygen changed to carbon

--- iTechOxy/Server/controllers/oxygen/index.js
+++ iTechOxy/Server/controllers/oxygen/index.js
@@ -3,7 +3,7 @@ class OxygenController {
         console.log(req.body);
         global.models.state.create({
             device_id: req.body.device_id,
-            oxygen: req.body.oxygen,
+            carbon: req.body.carbon,
             temp: req.body.temp
         });
         res.status(200).json({message: 'Logged', success: true});
